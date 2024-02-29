222dc660fa8f595a693f3269b8d4d14429536ec6
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-29 13:41:36 +0300

    Initial Commit

---
+++ ImageGeneratorApp/ImageGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorAppApp.swift
+//  ImageGeneratorApp
+//
+//  Created by Vefa Kosova on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PromptView()
+        }
+    }
+}
